10bddf49e05841df2273c5e5e11c4fcf9150870d
jack <jackwoodland963@gmail.com>
2025-04-10 10:51:16 +0000

    Updated Kernel File

--- src/kernel.c
+++ src/kernel.c
@@ -1,10 +1,12 @@
 void kernel_main() {
 	const char *message = "Eris Booting...";
-	char *video = (char *) 0xB8000;				// VGA BUFFER START
+	volatile char *video = (volatile char *) 0xB8000;
+
 	for (int i = 0; message[i] != '\0'; i++) {
-		video[i * 2] = message[i];			// CHARACTER
-		video[i * 2 + 1] = 0x07;			// ATTRIBUTE (WHITE TEXT ON BLACK BACKGROUND)
+		video[i * 2] = message[i];
+		video[i * 2 + 1] = 0x07;
 	}
+
 	while(1) {
 		
 	}
